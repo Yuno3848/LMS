424af1c1e047d7a7442469e0833bd1e9b60eb11e
Yuno3848 <bhandarianurag1234@gmail.com>
2025-07-11 01:54:23 +0530

    updated auth model & added studentProfile model

---
+++ backend/models/Users/studentProfile.js
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+
+const studentProfileSchema = new Schema({
+  isVerifiedStudent: {
+    type: Boolean,
+    default: false,
+  },
+  enrolledCourses: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+    },
+  ],
+  enrolledHistory: [
+    {
+      courseId: mongoose.Schema.Types.ObjectId,
+      ref: "Enrollment",
+    },
+  ],
+});
+
+const studentProfile = mongoose.model("studentProfile", studentProfileSchema);
+export default studentProfile;
