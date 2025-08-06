ba5649ae6663bf30a9046067b5cf69bc2d6d718c
Yuno3848 <bhandarianurag1234@gmail.com>
2025-08-06 09:17:59 +0530

    added trasanction model & transaction controller

---
+++ backend/controllers/enrollment.controller.js
@@ -0,0 +1,8 @@
+import { asyncHandler } from '../utils/asyncHandler.js';
+
+export const enrollInCourse = asyncHandler(async (req, res) => {});
+export const getUserEnrollments = asyncHandler(async (req, res) => {});
+export const getItemSectionProgress = asyncHandler(async (req, res) => {});
+export const getSubItemSectionProgress = asyncHandler(async (req, res) => {});
+export const checkEnrollment = asyncHandler(async (req, res) => {});
+export const markEnrollmentPaid = asyncHandler(async (req, res) => {});
---
+++ backend/models/transaction.model.js
@@ -0,0 +1,62 @@
+import mongoose, { Schema } from 'mongoose';
+
+const transactionSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+
+    courseId: {
+      type: Schema.Types.ObjectId,
+      ref: 'Course',
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+
+    currency: {
+      type: String,
+      enum: ['USD', 'INR'],
+      default: 'INR',
+    },
+    status: {
+      type: String,
+      enum: ['PENDING', 'COMPLETED', 'FAILED'],
+      default: 'PENDING',
+    },
+    paypalOrderId: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    payerID: {
+      type: String,
+    },
+    paymentGateway: {
+      type: String,
+      default: 'paypal',
+    },
+
+    email: {
+      type: String,
+    },
+    contact: {
+      type: String,
+    },
+
+    transactionDate: {
+      type: Date,
+      default: Date.now,
+    },
+    rawResponse: {
+      type: Object,
+    },
+  },
+  { timestamps: true },
+);
+
+export const Transaction = mongoose.model('Transaction', transactionSchema);
