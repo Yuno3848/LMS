3d0335072ae692b3cf56abb4069bcc9f8e21ff67
Yuno3848 <bhandarianurag1234@gmail.com>
2025-09-22 16:03:36 +0530

    add setCreateInstructorProfile in slicers

---
+++ frontend/src/redux/slicers/instructorProfileSlicer.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+let initialState;
+
+const instructorProfileSlice = createSlice({
+  name: "instructorProfile",
+  initialState: {
+    profile: null,
+    loading: true,
+  },
+  reducers: {
+    setCreateInstructorProfile: (state, action) => {
+      state.profile = action.payload;
+      state.loading = false;
+    },
+
+  },
+});
