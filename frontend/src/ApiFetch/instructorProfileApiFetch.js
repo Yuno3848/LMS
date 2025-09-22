946a3478e2f20f89cbf0a780b311906ceae655bb
Yuno3848 <bhandarianurag1234@gmail.com>
2025-09-22 15:52:01 +0530

    add createInstructorProfile fetch in instructorProfilieAPIFetch

--- frontend/src/ApiFetch/instructorProfileApiFetch.js
+++ frontend/src/ApiFetch/instructorProfileApiFetch.js
@@ -1,2 +1,23 @@
 const baseURL = "http://localhost:8080/api/v1/instructorProfile";
 
+const instructorProfileAPIFetch = {
+  createInstructorProfile: async (credential) => {
+    try {
+      const res = await fetch(`${baseURL}/create-instructor-profile`, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(credential),
+      });
+      const data = res.json();
+      if (!res.ok) {
+        throw Error("Failed to create instructor profile");
+      }
+      return { success: true, data };
+    } catch (error) {
+      return { success: false, message: error.message };
+    }
+  },
+};
