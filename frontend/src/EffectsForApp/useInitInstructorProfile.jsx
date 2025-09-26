34f3d078ae026624fb36cee9a604bcd51683fd16
Yuno3848 <bhandarianurag1234@gmail.com>
2025-09-26 06:41:55 +0530

    added useInitInstructorProfile

--- frontend/src/EffectsForApp/useInitInstructorProfile.jsx
+++ frontend/src/EffectsForApp/useInitInstructorProfile.jsx
@@ -1,21 +1,34 @@
-import React from 'react'
-import { authApi } from '../ApiFetch/authApiFetch'
+import React from "react";
+import { authApi } from "../ApiFetch/authApiFetch";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  setClearInstructorProfile,
+  setInstructorLoading,
+  setInstructorProfile,
+} from "../redux/slicers/instructorProfileSlicer";
+import { logout } from "../redux/slicers/authSlicer";
 
 const useInitInstructorProfile = () => {
+  const dispatch = useDispatch();
 
-  useEffect(()=>{
-    const fetchInstructorProfile = async()=>{
-        const result = await authApi.
-    }
-  })
+  useEffect(() => {
+    const fetchInstructorProfile = async () => {
+      try {
+        const result = await authApi.getInstructorProfile();
+        if (result.success) {
+          dispatch(setInstructorProfile(result?.data));
+        } else {
+          dispatch(setClearInstructorProfile());
+        }
+      } catch (error) {
+        dispatch(logout());
+        dispatch(setClearInstructorProfile());
+      } finally {
+        dispatch(setInstructorLoading(false));
+      }
+    };
+    fetchInstructorProfile();
+  }, [dispatch]);
+};
 
-
-  return (
-    <div>
-      
-    </div>
-  )
-}
-
-export default useInitInstructorProfile
-
+export default useInitInstructorProfile;
