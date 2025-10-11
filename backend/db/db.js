2e670da8d13c852ef438f1363db4c3421f1ce1f0
Yuno3848 <bhandarianurag1234@gmail.com>
2025-10-11 19:16:22 +0530

    update db js

--- backend/db/db.js
+++ backend/db/db.js
@@ -4,7 +4,7 @@ console.log('inside sign up');
 dotenv.config();
 const db = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       serverSelectionTimeoutMS: 10000,
--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,18 @@
 import db from './db/db.js';
 import app from './app.js';
 import swaggerDocs from './swagger/swagger.js';
+
 const port = process.env.PORT || 3000;
 
 db()
   .then(() => {
     app.listen(port, () => {
-      `Database Connected...`;
-      `Server running on port : ${port}`;
+      console.log('✅ Database Connected...');
+      console.log(`🚀 Server running on port : ${port}`);
       swaggerDocs(app, port);
     });
   })
   .catch((error) => {
-    ('Failed to connect to the database', error.message);
+    console.error('❌ Failed to connect to the database:', error.message);
     process.exit(1);
   });
