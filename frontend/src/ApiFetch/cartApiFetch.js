e1ba5e9c457babba45c600874750b06afc9fea85
Yuno3848 <bhandarianurag1234@gmail.com>
2025-11-09 16:47:39 +0530

    update cartApiFetch ;- removeFromCart headers

---
+++ frontend/src/ApiFetch/cartApiFetch.js
@@ -0,0 +1,58 @@
+const baseURL = `${import.meta.env.VITE_API_URL}/cart`;
+export const cartApiFetch = {
+  addToCart: async (courseId) => {
+    try {
+      const res = await fetch(`${baseURL}/add-to-cart/${courseId}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log("res ok :", res);
+        throw new Error(`failed to add in cart`);
+      }
+      return { success: true, data };
+    } catch (error) {
+      return { success: false, error: error.message };
+    }
+  },
+
+  removeFromCart: async (courseId) => {
+    try {
+      const res = await fetch(`${baseURL}/remove-from-cart`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify({ courseId }),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        throw new Error(`failed to remove from cart`);
+      }
+      return { success: true, data };
+    } catch (error) {
+      return { success: false, error: error.message };
+    }
+  },
+
+  showCart: async () => {
+    try {
+      const res = await fetch(`${baseURL}/show-cart`, {
+        method: "GET",
+        credentials: "include",
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        throw new Error(`failed to show cart`);
+      }
+      return { success: true, data };
+    } catch (error) {
+      return { success: false, error: error.message };
+    }
+  },
+};
