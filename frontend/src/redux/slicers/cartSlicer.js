d7b2c9fd82a710357252d44dea7812528054fcc5
Yuno3848 <bhandarianurag1234@gmail.com>
2025-11-09 16:54:47 +0530

    update cartSlicer - add setLoading, addCourse, removeCourse

--- frontend/src/redux/slicers/cartSlicer.js
+++ frontend/src/redux/slicers/cartSlicer.js
@@ -1,26 +1,30 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+  items: [],
+  loading: false,
+};
+
 const cartSlice = createSlice({
   name: "cart",
-  initialState: {
-    items: [],
-  },
+  initialState,
   reducers: {
     addCourse: (state, action) => {
-      const newCourse = action.payload;
-
-      const itemExist = state.items.find((item) => item.id == newCourse.id);
-
-      if (!itemExist) {
-        state.items.push(newCourse);
-      }
+      state.items = action.payload;
     },
-    removeCourse: (state, action) => {},
+    removeCourse: (state, action) => {
+
+    state.items = state.items.filter((item)=>item._id !== action.payload)
+
+    },
 
     setQuantity: () => {},
-    clearCart: () => {},
+    clearCart: () => ({ ...initialState }),
+    setLoading: (state, action) => {
+      state.loading = action.payload;
+    },
   },
 });
 
-export const { addCourse } = cartSlice.actions;
+export const { addCourse, setLoading, clearCart, removeCourse } = cartSlice.actions;
 export default cartSlice.reducer;
