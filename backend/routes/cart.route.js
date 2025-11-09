6ede38551fda009993252ad902165e355aebf3a6
Yuno3848 <bhandarianurag1234@gmail.com>
2025-11-09 16:43:34 +0530

    update route - validateRemoveCart()

--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -1,18 +1,15 @@
 import { Router } from 'express';
 import { validatorError } from '../middlewares/validatorError.js';
 import { isLogged } from '../middlewares/isLogged.middleware.js';
-import { validateCart } from '../validators/cart.validator.js';
-import { addToCart, removeFromCart } from '../controllers/cart.controller.js';
+import { validateCart, validateRemoveCart } from '../validators/cart.validator.js';
+import { addToCart, removeFromCart, showCart } from '../controllers/cart.controller.js';
 
 const cart = Router();
 
 cart.post('/add-to-cart/:courseId', isLogged, validateCart(), validatorError, addToCart);
 
-cart.delete(
-  '/remove-from-cart/:courseId',
-  isLogged,
-  validateCart(),
-  validatorError,
-  removeFromCart,
-);
+cart.delete('/remove-from-cart', isLogged, validateRemoveCart(), validatorError, removeFromCart);
+
+cart.get('/show-cart', isLogged, showCart);
+
 export default cart;
