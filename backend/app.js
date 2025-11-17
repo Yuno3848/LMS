dddaa14f71fb4a3c3921f16683e0306467e3c946
Yuno3848 <bhandarianurag1234@gmail.com>
2025-11-18 00:04:09 +0530

    update cors

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,11 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'http://localhost:8080'],
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:8080',
+      'https://lms-nine-green-57.vercel.app',
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
